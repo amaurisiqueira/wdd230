654e8625556bb0dd630cc4877a2a52f7f7c6eb15
amaurisiqueira <amauri.ferreira.siqeuira@gmail.com>
2024-03-10 01:29:40 -0300

    start chamber

---
+++ chamber/scripts/loadPages.js
@@ -0,0 +1,21 @@
+ // Función para cargar el contenido de las páginas "hijas"
+const myLoadPage=(pagina) =>{
+    fetch(pagina)
+        .then(response => {
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById('contenido').innerHTML = data;
+        })
+        .catch(error => {
+            console.log('Error loading page:', error);
+        });
+}
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    // Obtener el contenido de la página actual (por defecto, página principal)
+    myLoadPage('home.html');
+
+   
+});
